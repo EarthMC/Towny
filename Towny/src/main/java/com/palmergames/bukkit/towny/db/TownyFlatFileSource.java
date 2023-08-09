4a3b9af7a28fc2a4d2b17ee94b5eae7b8983da5c
Warrior <50800980+Warriorrrr@users.noreply.github.com>
2023-08-09 18:02:04 +0200

    Remove saving during flatfile loading

--- Towny/src/main/java/com/palmergames/bukkit/towny/db/TownyFlatFileSource.java
+++ Towny/src/main/java/com/palmergames/bukkit/towny/db/TownyFlatFileSource.java
@@ -605,8 +605,6 @@ public final class TownyFlatFileSource extends TownyDatabaseHandler {
 			} catch (Exception e) {
 				plugin.getLogger().log(Level.WARNING, Translation.of("flatfile_err_reading_resident_at_line", resident.getName(), line, resident.getName()), e);
 				return false;
-			} finally {
-				if (save) saveResident(resident);
 			}
 			return true;
 		} else {
@@ -1081,9 +1079,6 @@ public final class TownyFlatFileSource extends TownyDatabaseHandler {
 			} catch (Exception e) {
 				plugin.getLogger().log(Level.WARNING, Translation.of("flatfile_err_reading_town_file_at_line", town.getName(), line, town.getName()), e);
 				return false;
-			} finally {
-				if (town.exists())
-					saveTown(town);
 			}
 			return true;
 		} else {
@@ -1263,8 +1258,6 @@ public final class TownyFlatFileSource extends TownyDatabaseHandler {
 			} catch (Exception e) {
 				plugin.getLogger().log(Level.WARNING, Translation.of("flatfile_err_reading_nation_file_at_line", nation.getName(), line, nation.getName()), e);
 				return false;
-			} finally {
-				saveNation(nation);
 			}
 			return true;
 		} else {
